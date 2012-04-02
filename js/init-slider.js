e3265686e385e492325a461589c48afbb2b46a54
Luis Rodil-Fernandez <dropmeaword@gmail.com>
2012-04-02 03:12:01 +0200

    Added files necessary for gallery view in single post.

---
+++ js/init-slider.js
@@ -0,0 +1,7 @@
+/* on document load */
+jQuery(document).ready(function($) {
+	$('#coda-slider-1').codaSlider({
+													dynamicArrows: false,
+													dynamicTabs: false
+												});
+});
