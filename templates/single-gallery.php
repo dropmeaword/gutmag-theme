c8d2fcbfaddc9eecd6788ee6389d27036cef4370
mighil <tjemig@gmail.com>
2012-04-10 14:07:23 +0200

    Changed the single gallery bar
    I thinks it is better to have the controls of the gallery above the
    images, in case the pictures are in a different format, the controls
    will stay at the same position.
    Also there is no need for a black bar. This was only the case in the
    index page.

--- templates/single-gallery.php
+++ templates/single-gallery.php
@@ -4,37 +4,36 @@ get_header();
 ?> 
 
 <div class="container">  
-	<div id="content">
+<!--	<div id="content">-->
+
+	<div id="sliderControllergallery">
+		<span class="gallery_counter">1/13</span><span class="gallery_counter"> &nbsp;|&nbsp; </span>
+		<div class="coda-nav-left" id="coda-nav-left-1"><a href="#" title="Previous">previous</a> /&nbsp; </div> 
+		<div class="coda-nav-right" id="coda-nav-right-1"><a href="#" title="Next"> next</a>&nbsp;|</div>	
+		<div class="gallery_description">Photo descriptions can beplaced here.</div>
+	</div>
 
 	<div class="coda-slider-wrapper">
 		<div class="coda-slider preload" id="coda-slider-1">
 
-<? /* then we get all other images attached to this post */ ?>
-<?
-$imagetags = get_all_gallery_images( get_the_ID() );
-if( $imagetags && !empty($imagetags) ):
-	foreach($imagetags as $itag):
-?>
-				<div class="panel">
-					<div class="panel-wrapper">
-						<?= $itag ?>
-					</div>
+			<? /* then we get all other images attached to this post */ ?>
+			<?
+			$imagetags = get_all_gallery_images( get_the_ID() );
+			if( $imagetags && !empty($imagetags) ):
+				foreach($imagetags as $itag):
+			?>
+			<div class="panel">
+				<div class="panel-wrapper">
+					<?= $itag ?>
 				</div>
-<? 
-	endforeach;
-endif; 
-?>
+			</div>
+			<? 
+				endforeach;
+			endif; 
+			?>
 		</div><!-- .coda-slider -->
-
-		<div id="sliderController gallery">
-			<div class="coda-nav-left" id="coda-nav-left-1"><a href="#" title="Previous">previous</a></div>
-			<div class="coda-nav-right" id="coda-nav-right-1"><a href="#" title="Next">next</a></div>	
-			<span class="gallery_counter">1/13</span><span class="gallery_counter"> _ </span>
-			<div class="gallery_description">Photo descriptions can beplaced here.</div><br>
-		</div>
-
 	</div> <!-- /coda-slider-wrapper -->
-	</div> <!-- /content -->
+<!--	</div>--> <!-- /content -->
 <!-- sidebar would go here -->
 </div> <!-- /container -->
 
