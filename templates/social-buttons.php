a199803bd6be6e41e95fb5a56a416c944dc77351
Luis Rodil-Fernandez <dropmeaword@gmail.com>
2012-04-12 17:20:43 +0200

    Added link to the mag's facebook page

--- templates/social-buttons.php
+++ templates/social-buttons.php
@@ -1,12 +1,10 @@
 <div class="itemsocial">
 	<ul>
-		<li>
-			<a class="social_facebook" href="http://www.facebook.com" target="_blank"><img src="<? bloginfo('template_url'); ?>/icon/facebook.png"></a>
-		</li>
+		<li><a class="social_facebook" href="http://www.facebook.com/gutmag" target="_blank">yes, we are on facebook too</a></li>
+			<!--
 		<li>
 			<a class="social_twitter" href="http://twitter.com" target="_blank"><img src="<? bloginfo('template_url'); ?>/icon/twitter.png"></a>
 		</li>
-		<!--
 		<li>
 			<a class="social_gplus" href="http://twitter.com" target="_blank"><img src="<? bloginfo('template_url'); ?>/icon/google.png"></a>
 		</li>
